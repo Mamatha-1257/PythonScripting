9794b6c5900a97d5d592ad96bf347a6845f55f05
Mamatha-1257 <18wh1a1257@bvrithyderabad.edu.in>
2021-04-23 19:34:39 +0530

    Clean coding

--- resizeCrop.py
+++ resizeCrop.py
@@ -1,31 +1,28 @@
 import os
 from PIL import Image
 
-path = "/home/mamatha/Desktop/SampleData/"
-newPath = "/home/mamatha/Desktop/cropData/"
-intPath = "/home/mamatha/Desktop/resizeData/"
-finalPath = "/home/mamatha/Desktop/final/"
-left = 360
-top = 10
-right = 600
-bottom = 470
+SRC = "/home/mamatha/Desktop/SampleData/"
+DES = "/home/mamatha/Desktop/final/"
 
-for img in os.listdir(path):
+LEFT = 360
+TOP = 10
+RIGHT = 600
+BOTTOM = 470
+
+for img in os.listdir(SRC):
     
-    im = Image.open(path + img)
+    im = Image.open(SRC + img)
     im.show()
 
-    resizeImg = im.resize((round(im.size[0]*0.5), round(im.size[1]*0.5)))
-    rename =  intPath + img.split('.')[0] + "x" + ".png"
-    resizeImg.save(rename, optimize=True,quality=100)
-    resizeImg.show()
+    resizedImg = im.resize((round(im.size[0]*0.5), round(im.size[1]*0.5)),Image.ANTIALIAS)
+    resizedImg.show()
 
-    cropImg = resizeImg.crop((left, top, right, bottom))
-    rename =  img.split('.')[0] + "y" + ".png"
-    cropImg.save(newPath+rename, optimize=True,quality=100)
-    cropImg.show()
+    croppedImg = resizedImg.crop((LEFT, TOP, RIGHT, BOTTOM))
+    croppedImg.show()
 
-    finalImg = cropImg.resize((160, 300), Image.ANTIALIAS)
-    rename =  img.split('.')[0] + "z" + ".png"
-    finalImg.save(finalPath + rename, optimize=True, quality=95)
+    finalImg = croppedImg.resize((160, 300), Image.ANTIALIAS)
     finalImg.show()
+
+    rename =  img.split('.')[0] + "z" + ".png"
+    
+    finalImg.save(DES + rename, optimize=True)
